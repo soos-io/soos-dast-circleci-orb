e69d2e9998d8bce9a53a295f08559ed9d7f097f4
SOOS-JAlvarez <92373106+SOOS-JAlvarez@users.noreply.github.com>
2022-05-05 11:43:29 -0300

    code review - tests still missing

--- README.md
+++ README.md
@@ -143,3 +143,19 @@ jobs:
 
 ## References
  - [ZAP](https://www.zaproxy.org/)
+
+
+### How to Publish An Update
+1. Merge pull requests with desired changes to the main branch.
+    - For the best experience, squash-and-merge and use [Conventional Commit Messages](https://conventionalcommits.org/).
+2. Find the current version of the orb.
+    - You can run `circleci orb info soos-io/dast | grep "Latest"` to see the current version.
+3. Create a [new Release](https://github.com/soos-io/soos-dast-circleci-orb/releases/new) on GitHub.
+    - Click "Choose a tag" and _create_ a new [semantically versioned](http://semver.org/) tag. (ex: v1.0.0)
+      - We will have an opportunity to change this before we publish if needed after the next step.
+4.  Click _"+ Auto-generate release notes"_.
+    - This will create a summary of all of the merged pull requests since the previous release.
+    - If you have used _[Conventional Commit Messages](https://conventionalcommits.org/)_ it will be easy to determine what types of changes were made, allowing you to ensure the correct version tag is being published.
+5. Now ensure the version tag selected is semantically accurate based on the changes included.
+6. Click _"Publish Release"_.
+    - This will push a new tag and trigger your publishing pipeline on CircleCI.
--- src/scripts/run-dast.sh
+++ src/scripts/run-dast.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /zap/ || return
-if [  "$SOOS_PROJECT_NAME" == "" ]; then
+if [ -z "$SOOS_PROJECT_NAME" ]; then
     SOOS_PROJECT_NAME="${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}"
 fi
 PARAMS="--clientId ${SOOS_CLIENT_ID} --apiKey ${SOOS_API_KEY} --projectName ${SOOS_PROJECT_NAME} --scanMode ${SOOS_SCAN_MODE} --apiURL ${SOOS_API_BASE_URL} --integrationName ${SOOS_INTEGRATION_NAME} --commitHash ${CIRCLE_SHA1} --branchName ${CIRCLE_BRANCH}"
@@ -10,43 +10,43 @@ fi
 if [  "$SOOS_AJAX_SPIDER" == "true" ]; then
     PARAMS+=" --ajaxSpider True"
 fi
-if [  "$SOOS_RULES" != "" ]; then
+if [ -z "$SOOS_RULES" ]; then
     PARAMS+=" --rules ${SOOS_RULES}"
 fi
-if [  "$SOOS_CONTEXT_FILE" != "" ]; then
+if [ -z "$SOOS_CONTEXT_FILE" ]; then
     PARAMS+=" --contextFile ${SOOS_CONTEXT_FILE}"
 fi
-if [  "$SOOS_CONTEXT_USER" != "" ]; then
+if [ -z "$SOOS_CONTEXT_USER" ]; then
     PARAMS+=" --contextUser ${SOOS_CONTEXT_USER}"
 fi
-if [  "$SOOS_FULL_SCAN_MINUTES" != "" ]; then
+if [ -z "$SOOS_FULL_SCAN_MINUTES" ]; then
     PARAMS+=" --fullScanMinutes ${SOOS_FULL_SCAN_MINUTES}"
 fi
-if [  "$SOOS_API_SCAN_FORMAT" != "" ]; then
+if [ -z "$SOOS_API_SCAN_FORMAT" ]; then
     PARAMS+=" --apiScanFormat ${SOOS_API_SCAN_FORMAT}"
 fi
-if [  "$SOOS_LEVEL" != "" ]; then
+if [ -z "$SOOS_LEVEL" ]; then
     PARAMS+=" --level ${SOOS_LEVEL}"
 fi
-if [  "$SOOS_BRANCH_URI" != "" ]; then
+if [ -z "$SOOS_BRANCH_URI" ]; then
     PARAMS+=" --branchUri ${SOOS_BRANCH_URI}"
 fi
-if [  "$SOOS_BUILD_URI" != "" ]; then
+if [ -z "$SOOS_BUILD_URI" ]; then
     PARAMS+=" --buildUri ${SOOS_BUILD_URI}"
 fi
-if [  "$SOOS_BUILD_VERSION" != "" ]; then
+if [ -z "$SOOS_BUILD_VERSION" ]; then
     PARAMS+=" --buildVersion ${SOOS_BUILD_VERSION}"
 fi
-if [  "$SOOS_OPERATING_ENVIRONMENT" != "" ]; then
+if [ -z "$SOOS_OPERATING_ENVIRONMENT" ]; then
     PARAMS+=" --operatingEnvironment ${SOOS_OPERATING_ENVIRONMENT}"
 fi
-if [  "$SOOS_ZAP_OPTIONS" != "" ]; then
+if [ -z "$SOOS_ZAP_OPTIONS" ]; then
     PARAMS+=" --zapOptions ${SOOS_ZAP_OPTIONS}"
 fi
-if [  "$SOOS_REQUEST_COOKIES" != "" ]; then
+if [ -z "$SOOS_REQUEST_COOKIES" ]; then
     PARAMS+=" --requestCookies ${SOOS_REQUEST_COOKIES}"
 fi
-if [  "$SOOS_REQUEST_HEADERS" != "" ]; then
+if [ -z "$SOOS_REQUEST_HEADERS" ]; then
     PARAMS+=" --requestHeader ${SOOS_REQUEST_HEADERS}"
 fi
 if [  "$SOOS_GENERATE_SARIF_REPORT" == "true" ]; then
