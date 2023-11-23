fa600f199840630017bf05ed1eed801a7544f213
SOOS-JAlvarez <jalvarez@soos.io>
2023-11-23 14:31:46 -0300

    optional params

--- src/scripts/run-dast.sh
+++ src/scripts/run-dast.sh
@@ -19,38 +19,40 @@ PARAMS=(
     "--integrationType" "$SOOS_INTEGRATION_TYPE"
     "--commitHash" "${CIRCLE_SHA1}"
     "--branchName" "${CIRCLE_BRANCH}"
-    "${SOOS_DEBUG:+--debug}"
-    "${SOOS_AJAX_SPIDER:+--ajaxSpider}"
-     "${SOOS_RULES:+--rules}" "${SOOS_RULES}"
-    "${SOOS_CONTEXT_FILE:+--contextFile}" "${SOOS_CONTEXT_FILE}"
-    "${SOOS_FULL_SCAN_MINUTES:+--fullScanMinutes}" "${SOOS_FULL_SCAN_MINUTES}"
-    "${SOOS_API_SCAN_FORMAT:+--apiScanFormat}" "${SOOS_API_SCAN_FORMAT}"
-    "${SOOS_LOG_LEVEL:+--logLevel}" "${SOOS_LOG_LEVEL}"
-    "${SOOS_BRANCH_URI:+--branchUri}" "${SOOS_BRANCH_URI}"
-    "${SOOS_BUILD_URI:+--buildUri}" "${SOOS_BUILD_URI}"
-    "${SOOS_BUILD_VERSION:+--buildVersion}" "${SOOS_BUILD_VERSION}"
-    "${SOOS_OPERATING_ENVIRONMENT:+--operatingEnvironment}" "${SOOS_OPERATING_ENVIRONMENT}"
-    "${SOOS_ZAP_OPTIONS:+--zapOptions}" "${SOOS_ZAP_OPTIONS}"
-    "${SOOS_REQUEST_COOKIES:+--requestCookies}" "${SOOS_REQUEST_COOKIES}"
-    "${SOOS_REQUEST_HEADERS:+--requestHeaders}" "${SOOS_REQUEST_HEADERS}"
-    "${SOOS_BEARER_TOKEN:+--bearerToken}" "${SOOS_BEARER_TOKEN}"
-    "${SOOS_AUTH_USERNAME:+--authUsername}" "${SOOS_AUTH_USERNAME}"
-    "${SOOS_AUTH_PASSWORD:+--authPassword}" "${SOOS_AUTH_PASSWORD}"
-    "${SOOS_AUTH_LOGIN_URL:+--authLoginURL}" "${SOOS_AUTH_LOGIN_URL}"
-    "${SOOS_AUTH_USERNAME_FIELD:+--authUsernameField}" "${SOOS_AUTH_USERNAME_FIELD}"
-    "${SOOS_AUTH_PASSWORD_FIELD:+--authPasswordField}" "${SOOS_AUTH_PASSWORD_FIELD}"
-    "${SOOS_AUTH_SUBMIT_FIELD:+--authSubmitField}" "${SOOS_AUTH_SUBMIT_FIELD}"
-    "${SOOS_AUTH_SUBMIT_ACTION:+--authSubmitAction}" "${SOOS_AUTH_SUBMIT_ACTION}"
-    "${SOOS_OAUTH_TOKEN_URL:+--oauthTokenUrl}" "${SOOS_OAUTH_TOKEN_URL}"
-    "${SOOS_OAUTH_PARAMETERS:+--oauthParameters}" "${SOOS_OAUTH_PARAMETERS}"
-    "${SOOS_AUTH_SECOND_SUBMIT_FIELD:+--authSecondSubmitField}" "${SOOS_AUTH_SECOND_SUBMIT_FIELD}"
-    "${SOOS_AUTH_FORM_TYPE:+--authFormType}" "${SOOS_AUTH_FORM_TYPE}"
-    "${SOOS_AUTH_DELAY_TIME:+--authDelayTime}" "${SOOS_AUTH_DELAY_TIME}"
-    "${SOOS_DISABLE_RULES:+--disableRules}" "${SOOS_DISABLE_RULES}"
-    "${SOOS_AUTH_VERIFICATION_URL:+--authVerificationURL}" "${SOOS_AUTH_VERIFICATION_URL}"
-    "${SOOS_OTHER_OPTIONS:+--otherOptions}" "${SOOS_OTHER_OPTIONS}"
-    "${SOOS_VERBOSE:+--verbose}"
 )
+
+# Add optional parameters only if they are set and non-empty
+[ -n "${SOOS_DEBUG}" ] && PARAMS+=("--debug")
+[ -n "${SOOS_AJAX_SPIDER}" ] && PARAMS+=("--ajaxSpider" "${SOOS_AJAX_SPIDER}")
+[ -n "${SOOS_RULES}" ] && PARAMS+=("--rules" "${SOOS_RULES}")
+[ -n "${SOOS_CONTEXT_FILE}" ] && PARAMS+=("--contextFile" "${SOOS_CONTEXT_FILE}")
+[ -n "${SOOS_FULL_SCAN_MINUTES}" ] && PARAMS+=("--fullScanMinutes" "${SOOS_FULL_SCAN_MINUTES}")
+[ -n "${SOOS_API_SCAN_FORMAT}" ] && PARAMS+=("--apiScanFormat" "${SOOS_API_SCAN_FORMAT}")
+[ -n "${SOOS_LOG_LEVEL}" ] && PARAMS+=("--logLevel" "${SOOS_LOG_LEVEL}")
+[ -n "${SOOS_BRANCH_URI}" ] && PARAMS+=("--branchUri" "${SOOS_BRANCH_URI}")
+[ -n "${SOOS_BUILD_URI}" ] && PARAMS+=("--buildUri" "${SOOS_BUILD_URI}")
+[ -n "${SOOS_BUILD_VERSION}" ] && PARAMS+=("--buildVersion" "${SOOS_BUILD_VERSION}")
+[ -n "${SOOS_OPERATING_ENVIRONMENT}" ] && PARAMS+=("--operatingEnvironment" "${SOOS_OPERATING_ENVIRONMENT}")
+[ -n "${SOOS_ZAP_OPTIONS}" ] && PARAMS+=("--zapOptions" "${SOOS_ZAP_OPTIONS}")
+[ -n "${SOOS_REQUEST_COOKIES}" ] && PARAMS+=("--requestCookies" "${SOOS_REQUEST_COOKIES}")
+[ -n "${SOOS_REQUEST_HEADERS}" ] && PARAMS+=("--requestHeaders" "${SOOS_REQUEST_HEADERS}")
+[ -n "${SOOS_BEARER_TOKEN}" ] && PARAMS+=("--bearerToken" "${SOOS_BEARER_TOKEN}")
+[ -n "${SOOS_AUTH_USERNAME}" ] && PARAMS+=("--authUsername" "${SOOS_AUTH_USERNAME}")
+[ -n "${SOOS_AUTH_PASSWORD}" ] && PARAMS+=("--authPassword" "${SOOS_AUTH_PASSWORD}")
+[ -n "${SOOS_AUTH_LOGIN_URL}" ] && PARAMS+=("--authLoginURL" "${SOOS_AUTH_LOGIN_URL}")
+[ -n "${SOOS_AUTH_USERNAME_FIELD}" ] && PARAMS+=("--authUsernameField" "${SOOS_AUTH_USERNAME_FIELD}")
+[ -n "${SOOS_AUTH_PASSWORD_FIELD}" ] && PARAMS+=("--authPasswordField" "${SOOS_AUTH_PASSWORD_FIELD}")
+[ -n "${SOOS_AUTH_SUBMIT_FIELD}" ] && PARAMS+=("--authSubmitField" "${SOOS_AUTH_SUBMIT_FIELD}")
+[ -n "${SOOS_AUTH_SUBMIT_ACTION}" ] && PARAMS+=("--authSubmitAction" "${SOOS_AUTH_SUBMIT_ACTION}")
+[ -n "${SOOS_OAUTH_TOKEN_URL}" ] && PARAMS+=("--oauthTokenUrl" "${SOOS_OAUTH_TOKEN_URL}")
+[ -n "${SOOS_OAUTH_PARAMETERS}" ] && PARAMS+=("--oauthParameters" "${SOOS_OAUTH_PARAMETERS}")
+[ -n "${SOOS_AUTH_SECOND_SUBMIT_FIELD}" ] && PARAMS+=("--authSecondSubmitField" "${SOOS_AUTH_SECOND_SUBMIT_FIELD}")
+[ -n "${SOOS_AUTH_FORM_TYPE}" ] && PARAMS+=("--authFormType" "${SOOS_AUTH_FORM_TYPE}")
+[ -n "${SOOS_AUTH_DELAY_TIME}" ] && PARAMS+=("--authDelayTime" "${SOOS_AUTH_DELAY_TIME}")
+[ -n "${SOOS_DISABLE_RULES}" ] && PARAMS+=("--disableRules" "${SOOS_DISABLE_RULES}")
+[ -n "${SOOS_AUTH_VERIFICATION_URL}" ] && PARAMS+=("--authVerificationURL" "${SOOS_AUTH_VERIFICATION_URL}")
+[ -n "${SOOS_OTHER_OPTIONS}" ] && PARAMS+=("--otherOptions" "${SOOS_OTHER_OPTIONS}")
+[ -n "${SOOS_VERBOSE}" ] && PARAMS+=("--verbose")
 
 set -x
 node dist/index.js "${SOOS_TARGET_URL}" "${PARAMS[@]}"
